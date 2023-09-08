030df40f446ad47d02a9543699f9d22d856985dc
Fridho Kesuma Pratama <fridho.pratama@bankmega.com>
2023-09-08 14:52:51 +0700

    Alternative: using the custom store

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import ProductItem from "../components/Products/ProductItem";
+import { useStore } from "../store/store";
 import "./Products.css";
-import { ProductsContext } from "../context/products-context";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
